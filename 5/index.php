98fd2ed89e67c4086742aef1547b728a97ea4076
hugo <tonEmail@gmail.com>
2025-11-19 14:19:14 +0100

    first commit

--- 5/index.php
+++ 5/index.php
@@ -1,42 +1,95 @@
 <?php
- 
 try {
-$mysqlClient =  new PDO(
-    'mysql:host=localhost;dbname=jo;charset=utf8',
-    'root',
-    ''
-);
-} catch(PDOexception $e){
+    $mysqlClient = new PDO(
+        'mysql:host=localhost;dbname=jo;charset=utf8',
+        'root',
+        ''
+    );
+} catch(PDOException $e){
     die($e->getMessage());
 }
- 
-$query = $mysqlClient->prepare("select * from jo.`100`;");
+
+// ----------------------------
+// Gestion du tri ($_GET)
+// ----------------------------
+$sort = "nom";   // tri par défaut
+if (isset($_GET['sort'])) {
+    $sort = $_GET['sort'];
+}
+
+$order = "asc";  // ordre par défaut
+if (isset($_GET['order'])) {
+    $order = $_GET['order'];
+}
+
+// Whitelist pour éviter les injections SQL
+$allowedSort = ["nom", "pays", "course", "temps"];
+$allowedOrder = ["asc", "desc"];
+
+if (!in_array($sort, $allowedSort)) {
+    $sort = "nom";
+}
+
+if (!in_array($order, $allowedOrder)) {
+    $order = "asc";
+}
+
+// Requête SQL avec ORDER BY
+$sql = "SELECT * FROM jo.`100` ORDER BY $sort $order";
+
+$query = $mysqlClient->prepare($sql);
 $query->execute();
- 
 $data = $query->fetchAll();
- 
+
 $mysqlClient = null;
-$dbh = null;
 ?>
-<table>
-    <thead>
-        <tr>
-            <th>Nom</th>
-            <th>Pays</th>
-            <th>Course</th>
-            <th>Temps</th>
-        </tr>
-    </thead>
-<?php foreach($data as $value) { ?>
-    <tr>
-        <td><?php echo $value["nom"]; ?></td>
-        <td><?php echo $value["pays"]; ?></td>
-        <td><?php echo $value["course"]; ?></td>
-        <td><?php echo $value["temps"]; ?></td>
-    </tr>
-<?php }  ?>
-</table>
-<?php
+<h2>Trier par :</h2>
+
+<p>
+    Nom :
+    <a href="?sort=nom&order=asc">↑</a>
+    <a href="?sort=nom&order=desc">↓</a>
+</p>
+
+<p>
+    Pays :
+    <a href="?sort=pays&order=asc">↑</a>
+    <a href="?sort=pays&order=desc">↓</a>
+</p>
+
+<p>
+    Course :
+    <a href="?sort=course&order=asc">↑</a>
+    <a href="?sort=course&order=desc">↓</a>
+</p>
+
+<p>
+    Temps :
+    <a href="?sort=temps&order=asc">↑</a>
+    <a href="?sort=temps&order=desc">↓</a>
+</p>
+
+<hr>
+
+<h2>Résultats :</h2>
+
+<?php foreach ($data as $value): ?>
+
+<p>
+    <strong>Nom :</strong> <?= $value["nom"] ?><br>
+    <strong>Pays :</strong> <?= $value["pays"] ?><br>
+    <strong>Course :</strong> <?= $value["course"] ?><br>
+    <strong>Temps :</strong> <?= $value["temps"] ?><br>
+</p>
+
+<hr>
+
+<?php endforeach; ?>
+
+
+
+
+
  
  
  
